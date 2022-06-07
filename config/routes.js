c9ec301d0dc965d907041b48b071981d0ef71b3f
Marcos Rampaso <49240709+Mazner@users.noreply.github.com>
2022-06-07 12:34:20 -0300

    Add files via upload

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,18 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.get)
-}
+
+    app.route('/servicos')
+        .get(app.api.servicos.get)
+        .post(app.api.servicos.save)
+        .delete(app.api.servicos.remove)
+
+    app.route ('/servicos/tree')
+        .get(app.api.servicos.getTree)
+
+ /*   app.route('/servicos/:id')
+        .get(app.api.category.getById)      debugar dps
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+    */
+    }
